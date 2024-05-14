5de4c622a9c20b4bfecba07910eac33f3bfb2efc
tlsuppliertemplates <138880302+tlsuppliertemplates@users.noreply.github.com>
2024-05-14 05:16:37 -0500

    Add files via upload

---
+++ ksi/js/uag-plugin-assets-0-uag-js-263.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-oir5delc' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-a0be6mty' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-5e10197c' );
+});
+ });
